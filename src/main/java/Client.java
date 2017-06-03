5809b420f215c40b1bf26a6c498033937665ec1f
liukxaa <644306737@qq.com>
2017-06-03 13:15:44 +0800

    新增 界面布局

---
+++ src/main/java/Client.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ * Created by Roman on 2017/6/3.
+ */
+public class Client {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        GameOfLife gameOfLife = new GameOfLife();
+        gameOfLife.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameOfLife.setVisible(true);
+    }
+}
--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -1,8 +1,33 @@
+
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Roman on 2017/6/3.
  */
-public class GameOfLife {
-    public static int[][] getFrames(int width, int height) {
-        return new int[width][height];
+public class GameOfLife extends JFrame {
+
+    private JPanel allPanel = new JPanel(new BorderLayout());
+    private JPanel leftPanel = new JPanel();
+    private JPanel rightPanel = new JPanel(new GridLayout(4, 1));
+
+    private JButton randomButton = new JButton("生成细胞");
+    private JButton startButton = new JButton("开  始");
+    private JButton stopButton = new JButton("结  束");
+
+    private JTextField frameSizeText = new JTextField();
+
+    public GameOfLife() {
+        setSize(800, 500);
+        leftPanel.setSize(600, 500);
+        rightPanel.setSize(200, 500);
+        rightPanel.add(frameSizeText);
+        rightPanel.add(randomButton);
+        rightPanel.add(startButton);
+        rightPanel.add(stopButton);
+        allPanel.add(leftPanel, BorderLayout.WEST);
+        allPanel.add(rightPanel, BorderLayout.EAST);
+        add(allPanel);
     }
+
 }
