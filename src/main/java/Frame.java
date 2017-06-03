8469b7c1c7bda8b85fd4c5f772ac450e9e7afaa7
liukxaa <644306737@qq.com>
2017-06-03 11:22:32 +0800

    新增 获得某个细胞领域中生存细胞的个数及其测试函数

--- src/main/java/Frame.java
+++ src/main/java/Frame.java
@@ -5,16 +5,15 @@ public class Frame {
     private int height = 3;
     private int width = 3;
     private String[][] frames;
+    private static final int BORDER_NUM = 8;
+    private static final String LIVE = "1";
+    private static final String DEAD = "0";
+    private int[][] border = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
 
     public Frame(int width, int height) {
         this.width = width;
         this.height = height;
         this.frames = new String[width][height];
-    }
-
-
-    public String[][] getFrames() {
-        return this.frames;
     }
 
     public int getWidth() {
@@ -25,15 +24,47 @@ public class Frame {
         return this.height;
     }
 
-    public int getNeighborsNumOf(int indexOfWidth, int indexOfHeight) {
-        return 0;
+    public int getLiveNeighborsNumOf(int indexOfWidth, int indexOfHeight) {
+        int liveNumSum = 0;
+        for (int i = 0; i < BORDER_NUM; i++) {
+            int positionX = indexOfWidth + border[i][0];
+            int positionY = indexOfHeight + border[i][1];
+            if (isInBorder(positionX, positionY) && LIVE.equals(frames[positionX][positionY])) {
+                liveNumSum++;
+            }
+            if (liveNumSum >= 3) {
+                return liveNumSum;
+            }
+        }
+        return liveNumSum;
+    }
+
+    private boolean isInBorder(int positionX, int positionY) {
+        return (positionX >= 0 && positionX < width) && (positionY >= 0 && positionY < height);
     }
 
     public void setLive(int indexOfWidth, int indexOfHeight) {
-        this.frames[indexOfHeight][indexOfHeight] = "1";
+        this.frames[indexOfWidth][indexOfHeight] = LIVE;
     }
 
     public String getResult() {
-        return "0 0 0 1 1 1 0 1 0";
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                answer.append(this.frames[i][j]);
+            }
+        }
+        return answer.toString();
+    }
+
+    public void setFrame(String frame) {
+        String[] lines = frame.split("\\|");
+        for (int row = 0; row < lines.length; row++) {
+            String line = lines[row];
+            String[] cols = line.split("");
+            for (int col = 0; col < cols.length; col++) {
+                this.frames[row][col] = cols[col];
+            }
+        }
     }
 }
--- src/test/java/GameOfLifeTest.java
+++ src/test/java/GameOfLifeTest.java
@@ -8,14 +8,29 @@ public class GameOfLifeTest {
     public void should_live_if_has_3_neighbors() {
 
         Frame frame = new Frame(3, 3);
+        frame.setFrame("000|101|010");
         for (int i = 0; i < frame.getWidth(); i++) {
             for (int j = 0; j < frame.getHeight(); j++) {
-                if (frame.getNeighborsNumOf(i, j) >= 3) {
+                if (frame.getLiveNeighborsNumOf(i, j) >= 3) {
                     frame.setLive(i, j);
                 }
             }
         }
 
-        assertEquals("0 0 0 1 1 1 0 1 0", frame.getResult());
+        assertEquals("000111010", frame.getResult());
+    }
+
+    @Test
+    public void should_get_1_if_one_has_1_live_neighbors() {
+        Frame frame = new Frame(3, 3);
+        frame.setFrame("000|101|010");
+        assertEquals(1, frame.getLiveNeighborsNumOf(0, 0), 0);
+    }
+
+    @Test
+    public void should_get_3_if_one_has_3_live_neighbors() {
+        Frame frame = new Frame(3, 3);
+        frame.setFrame("000|101|010");
+        assertEquals(3, frame.getLiveNeighborsNumOf(1, 1), 0);
     }
 }
